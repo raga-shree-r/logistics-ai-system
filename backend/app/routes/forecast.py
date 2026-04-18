25d3220b1ca7534a88fa996d53525027ba47f689
raga-shree-r <ragashree086@gmail.com>
2026-04-18 11:51:40 +0530

    Update forecast.py

--- backend/app/routes/forecast.py
+++ backend/app/routes/forecast.py
@@ -8,12 +8,11 @@ router = APIRouter()
 def get_forecast():
     result = run_forecast()
 
-    # ✅ FIX: extract values properly
     forecast_values = [item["predicted_demand"] for item in result]
     labels = [item["customer_id"] for item in result]
 
     return {
         "status": "success",
         "forecast": forecast_values,
-        "dates": labels   # frontend will use this as x-axis
+        "dates": labels
     }
